2ff23cd05ad0809b0fc155d6792007bcb9f704b9
Alex Christian <alexchristian1198@gmail.com>
2025-02-26 23:47:02 -0500

    changes in settings1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -113,6 +113,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / 'static/media'
 
 
 # Default primary key field type
